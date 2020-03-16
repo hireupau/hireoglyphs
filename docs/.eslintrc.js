2099225f4f6fc9f3c40e779cd4c115e1ec54789d
Scott de Jonge <dejonge.scott@gmail.com>
2020-03-16 15:23:50 +1100

    Add custom ESLint config

--- docs/.eslintrc.js
+++ docs/.eslintrc.js
@@ -1,17 +1,55 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
+    node: true,
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+    'plugin:@hireupau/hu-vue/recommended'
   ],
-  // add your custom rules here
   rules: {
-  }
-}
+    'import/no-extraneous-dependencies': 'error',
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'no-restricted-globals': [
+      'error',
+      {
+        'name': 'fetch',
+        'message': 'use methods from utils/http.js'
+      },
+    ],
+    'no-restricted-imports': ['error', { 'patterns': ['lodash*', 'moment*', 'axios'] }],
+    'vue/v-bind-style': ['error', 'longform'],
+    'vue/html-self-closing': ['error', {
+      'html': {
+        'void': 'always',
+        'normal': 'never',
+        'component': 'always',
+      },
+      'svg': 'always',
+      'math': 'always',
+    }],
+    'vue/v-on-style': ['error', 'longform'],
+    'vue/script-indent': ['error', 2, {
+      'baseIndent': 1,
+    }],
+    'no-unused-vars': ['error', {
+      'vars': 'all',
+      'args': 'all',
+      'argsIgnorePattern': '^_',
+      'ignoreRestSiblings': false,
+    }],
+  },
+  overrides: [{
+    files: ['*.vue'],
+    rules: {
+      indent: 'off'
+    },
+  }],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
