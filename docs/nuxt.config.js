df798559c818f05b1a311cffb6bfa74367f7d863
Scott de Jonge <dejonge.scott@gmail.com>
2021-08-03 17:29:40 +1000

    Styles

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -28,6 +28,19 @@ export default {
         type: 'image/x-icon',
         href: '/favicon.ico',
       },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+      {
+        rel: 'preconnect',
+        crossorigin: 'crossorigin',
+        href: 'https://fonts.gstatic.com',
+      },
+      {
+        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap',
+        rel: 'stylesheet',
+      },
     ],
   },
   loading: {
