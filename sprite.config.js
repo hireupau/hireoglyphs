679529123bfa561f77db2990df5252428b13f16d
Scott de Jonge <dejonge.scott@gmail.com>
2021-08-13 16:36:11 +1000

    Sprite config

---
+++ sprite.config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+  dest: './icons/',
+  transform: [],
+  mode: {
+    symbol: {
+      dest: './',
+      sprite: './sprite.svg',
+      inline: true,
+      example: false,
+    },
+  },
+};
