f744d2d98ef5fdf09e69e7dc04bac2a4c22aaaa1
Scott de Jonge <dejonge.scott@gmail.com>
2021-08-13 13:43:40 +1000

    Update SVGO

---
+++ svgo.config.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const { extendDefaultPlugins } = require('svgo');
+
+module.exports = {
+  floatPrecision: 2,
+  multipass: true,
+  js2svg: {
+    pretty: true,
+    indent: 2,
+  },
+  plugins: extendDefaultPlugins([
+    {
+      name: 'cleanupListOfValues'
+    },
+    {
+      name: 'removeAttrs',
+      params: {
+        attrs: [
+          'fill',
+        ],
+      },
+    },
+    {
+      name: 'removeUnknownsAndDefaults',
+    },
+    {
+      name: 'removeDimensions',
+    },
+    {
+      name: 'removeViewBox',
+    },
+    {
+      name: 'sortAttrs',
+    },
+  ]),
+};
