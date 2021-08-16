2bfee4d55bb2acbd89de6e90ef19c0bd4e7e29d9
Scott de Jonge <dejonge.scott@gmail.com>
2021-08-16 11:30:01 +1000

    Remove Nuxt.js use Vue-cli

---
+++ src/components/Icon.vue
@@ -0,0 +1,57 @@
+<script>
+  export default {
+    props: {
+      icon: {
+        type: Object,
+        required: true,
+      },
+      size: {
+        type: Number,
+        default: 24,
+      },
+    },
+    computed: {
+      symbolId() {
+        return `#svg--${this.icon.id}`;
+      },
+      downloadUrl() {
+        return `/icons/${this.icon.name}`;
+      },
+    },
+    methods: {
+      copyToClipboard(event) {
+        if (!navigator.clipboard) {
+          return;
+        }
+
+        const value = event.target.dataset.icon;
+        navigator.clipboard.writeText(value);
+      },
+    },
+  };
+</script>
+
+<template>
+  <div class="icon">
+    <div class="icon-inner">
+      <pre>{{ icon.id }}</pre>
+    </div>
+    <div class="icon-actions">
+      <button
+        ref="copy"
+        class="icon-copy"
+        v-bind:data-icon="icon.id"
+        v-on:click="copyToClipboard"
+      >
+        Copy<span class="visually-hidden"> to clipboard</span>
+      </button>
+      <a
+        class="icon-download"
+        v-bind:href="downloadUrl"
+        download
+      >
+        Download
+      </a>
+    </div>
+  </div>
+</template>
---
+++ src/components/Icons.vue
@@ -0,0 +1,33 @@
+<script>
+  import Icon from './Icon.vue';
+
+  export default {
+    components: {
+      Icon,
+    },
+    props: {
+      icons: {
+        type: Array,
+        required: true,
+      },
+      size: {
+        type: Number,
+        default: 24,
+      },
+    },
+  };
+</script>
+
+<template>
+  <ul class="grid">
+    <li
+      v-for="icon in icons"
+      v-bind:key="icon.id"
+    >
+      <Icon
+        v-bind:icon="icon"
+        v-bind:size="size"
+      />
+    </li>
+  </ul>
+</template>
