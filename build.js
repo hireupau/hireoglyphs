e310d2be4466e284711fe83fb685cbd9c073bfcb
Scott de Jonge <dejonge.scott@gmail.com>
2021-08-16 15:18:05 +1000

    Add aliases

--- build.js
+++ build.js
@@ -2,15 +2,16 @@
 
 'use strict'
 
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const { loadConfig, optimize } = require('svgo');
+const aliases = require('./aliases.json');
 
 async function directoryToObject(directory, done) {
   let results = [];
 
   fs.readdir(directory, async function(error, list) {
-    if (error) return done(error);
+    if (error) throw error;
 
     // Filter hidden files from directory e.g. .DS_STORE
     list = list.filter(item => !(/(^|\/)\.[^/.]/g).test(item));
@@ -20,10 +21,11 @@ async function directoryToObject(directory, done) {
 
       fs.stat(file, function(error) {
         if (error) throw error;
-
+        const id = path.basename(file, '.svg');
         results.push({
-          id: path.basename(file, '.svg'),
+          id,
           file: path.basename(file),
+          aliases: aliases[id],
         });
 
         if (!--list.length) {
@@ -35,7 +37,7 @@ async function directoryToObject(directory, done) {
 }
 
 async function directoryToJson(directory) {
-  await directoryToObject((directory), function(error, result) {
+  await directoryToObject(directory, function(error, result) {
     if (error) throw error;
     const data = JSON.stringify(result, null, 2);
     fs.writeFile('icons.json', data, (error) => {
@@ -46,20 +48,20 @@ async function directoryToJson(directory) {
 
 async function processFile(directory, file, config) {
   const filepath = path.resolve(directory, file);
-  const original = await fs.readFile(filepath, 'utf8');
+  const original = fs.readFileSync(filepath, 'utf8');
   const optimised = await optimize(original, {
     path: filepath,
     ...config
   }).data;
 
-  await fs.writeFile(filepath, optimised, 'utf8');
+  fs.writeFileSync(filepath, optimised, 'utf8');
 }
 
 (async () => {
   try {
     const basePath = path.join(__dirname, './icons/');
     const svgoConfig = await loadConfig(path.join(__dirname, 'svgo.config.js'));
-    const files = await fs.readdir(basePath);
+    const files = fs.readdirSync(basePath);
     await directoryToJson(basePath);
     await Promise.all(files.map(file => processFile(basePath, file, svgoConfig)));
   } catch (error) {
